4b70b51c57ecf545d1b9a804bbd147409a3f9170
Ritam Chatterjee <85054086+regnna@users.noreply.github.com>
2023-07-14 16:50:29 +0530

    hb

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -23,8 +23,8 @@ const Home = () => {
                 </div>
             </div>
 
-            <div className='my-10'>
-                <img src={HeroImage} alt='my Profile' className='rounded-2xl ml-auto mx-auto my-auto h-auto w-2/3 md:w-full'></img>
+            <div className='my-5'>
+                <img src={HeroImage} alt='my Profile' className='rounded-2xl ml-auto mx-auto my-auto h-auto w-2/3 md:w-full sm:mx-5'></img>
             </div>
         </div>
         
