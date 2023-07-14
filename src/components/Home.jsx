a2a88773448b1c7efa57027cc7aa3b77aecbe81d
Ritam Chatterjee <85054086+regnna@users.noreply.github.com>
2023-07-14 16:38:31 +0530

    ff

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
     <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 '>
         <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row '>
             <div className='flex flex-col justify-center h-full my-6'>
-                <h2 className='text-4xl sm:text-7xl font-bold text-yellow-50 justify-center px-3 my-6 '> A Data vigilante</h2>
+                <h2 className='text-4xl sm:text-7xl font-bold text-yellow-50 justify-center px-6 my-6'>  Data vigilante</h2>
                 <p className='text-gray-500 py-4 max-w-md'>
                 Machine Learning Enthusiast | AI Devotee | Problem Solver | Front-End Developer | Python Developer | Spring-Boot Seasoned
                 </p>
@@ -24,7 +24,7 @@ const Home = () => {
             </div>
 
             <div className='my-23'>
-                <img src={HeroImage} alt='my Profile' className='rounded-2xl ml-auto mx-auto h-auto w-2/3 md:w-full'></img>
+                <img src={HeroImage} alt='my Profile' className='rounded-2xl ml-auto mx-auto my-auto h-auto w-2/3 md:w-full'></img>
             </div>
         </div>
         
