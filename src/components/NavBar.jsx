33a8292a806e3fd3ba1572b8a266f3c6573bf02d
Ritam Chatterjee <85054086+regnna@users.noreply.github.com>
2023-07-23 01:13:42 +0530

    44

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -32,7 +32,7 @@ const NavBar = () => {
     <div className="flex justify-between items-center w-full h-20 px-4 bg-black fixed">
         <div>
             <h1 className="text-5xl font-signature ml-2 text-yellow-100">
-                <div className='animate-pulse'>Ritam</div>
+                <div className='animate-pulse'>Ritam Chatterjee</div>
             </h1>
         </div>
         
