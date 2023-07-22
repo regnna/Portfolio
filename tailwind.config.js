526f0f0dc01e1d9f06ffc3b0e57c64786cf4d2eb
Ritam Chatterjee <85054086+regnna@users.noreply.github.com>
2023-07-23 01:11:47 +0530

    225

--- tailwind.config.js
+++ tailwind.config.js
@@ -39,7 +39,7 @@ module.exports = {
       },
       animation: {
         'waving-hand': 'wiggle 2s linear infinite',
-        'img':'scaleImage 2s linear infinite',
+        'img':'scaleImage 5s linear infinite',
         'w-hand': 'wave 2s linear infinite',
       },
     },
