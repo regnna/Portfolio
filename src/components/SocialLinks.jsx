1c212efdf483c7a0be5313995aeb6c5e0f293e8f
regnna <ritmchats@gmail.com>
2023-08-17 19:06:43 +0530

    ggs

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -46,13 +46,13 @@ const SocialLinks = () => {
 
     ]
     return (
-        <div className="hidden lg:flex flex-col top-[35%] left-18  fixed ">
+        <div className="hidden lg:flex flex-col top-[35%] left-13  fixed ">
           <ul>
             {links.map(({ id, child, href, style, download }) => (
               <li
                 key={id}
                 className={
-                  "flex justify-between items-center w-40 h-14 px-4 ml-[-75px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" +
+                  "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" +
                   " " +
                   style
                 }
