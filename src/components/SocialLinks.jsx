5c206428f8d5f3ea3258424679fc931b24be6d80
regnna <ritmchats@gmail.com>
2023-08-17 20:46:41 +0530

    ggs

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -52,9 +52,8 @@ const SocialLinks = () => {
               <li
                 key={id}
                 className={
-                  "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" +
-                  " " +
-                  style
+                  `flex justify-between items-center w-40 h-14 px-4 ml-[-7vw] hover:ml-[-.7vw] hover:rounded-md duration-300 bg-gray-500 
+                  ${style}`
                 }
               >
                 <a
