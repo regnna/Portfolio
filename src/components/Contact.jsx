001baf1197a6a330d92f9436d448959dfcc63c57
regnna <ritmchats@gmail.com>
2023-08-29 19:57:15 +0530

    lets goo

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,8 +13,8 @@ const Contact = () => {
             <div className=' flex justify-center items-center'>
                 <form action='https://getform.io/f/20da7912-39d0-46fb-b4ff-d9c61e31cf7e' method='POST' className='flex flex-col w-full md:w-1/2'>
                     <input type='text' name='name' placeholder='Enter your name' className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'/>
-                    <input type='text' name='Email' placeholder='Enter your Email' className='my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'/>
-                    <textarea name='message'  rows="10" placeholder='Enter your Message' className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' ></textarea>
+                    <input type='text' name='Email' placeholder='Enter your Email' className='my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' required/>
+                    <textarea name='message'  rows="10" placeholder='Enter your Message' className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' required></textarea>
                     <button className='text-slate-950 font-bold bg-gradient-to-b from-yellow-500 via-yellow-100 to-orange-600  px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300'> Let's talk</button>
                 </form>
             </div>
