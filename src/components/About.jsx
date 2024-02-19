1246ba46f9df6ed58e98f283740b3b2a39267aaa
Ritam Chatterjee <85054086+regnna@users.noreply.github.com>
2024-02-19 13:44:59 +0530

    chore: about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,8 +9,9 @@ const About = () => {
                     About
                 </p>
             </div>
-            <div className='justify justify-center'>
-            <p className='text-xl mt-15 '>
+            {/* <div className='justify justify-center'> */}
+              
+            <p className='text-xl mt-15 text-center '>
               I am Ritam Chatterjee from West Bengal, India
               <br/>
               {/* <br/><div class="w-screen-35 bg-blue-300">
@@ -34,7 +35,7 @@ const About = () => {
             <p className='text-xl '>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
             </p> */}
-        </div>
+        {/* </div> */}
 
     </div>
   )
