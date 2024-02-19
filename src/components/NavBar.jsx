925bbbd90944b29b2ad854e76a357ab8d479cdbc
Ritam Chatterjee <85054086+regnna@users.noreply.github.com>
2024-02-19 14:54:48 +0530

    chore: nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -31,8 +31,8 @@ const NavBar = () => {
   return (
     <div className="flex justify-between items-center  w-full h-[5vw] px-[3vw] sm:pt-[1dvh]  md:pt-[3dvh] lg:pt-[1dvh] w-screen-30:pt-[1dvh] bg-black fixed">
         <div >
-            <div className="text-5xl font-signature  text-yellow-100">
-                <h1 className='animate-pulse mt-15 md:mt-0 lg:mt-0'>Ritam Chatterjee</h1>
+            <div className="text-5xl mt-15 font-signature  text-yellow-100">
+                <h1 className='animate-pulse  md:mt-0 lg:mt-0'>Ritam Chatterjee</h1>
             </div>
         </div>
         
